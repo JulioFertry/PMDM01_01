03cf48ecf43434805c23a3a5b3e130263c2cc6a7
JulioFertry <juliofertry@gmail.com>
2024-10-17 16:41:26 +0200

    ejFinal Kotlin

--- KotlinRepaso/src/main/kotlin/Main.kt
+++ KotlinRepaso/src/main/kotlin/Main.kt
@@ -12,7 +12,7 @@ fun ej03() {
 
 
 /** ej05 */
-class Personaje(val nombre: String, val vida: Int, val ataque: Int) {
+class Personaje(val nombre: String, var vida: Int, val ataque: Int) {
 
     fun mostrarInfo() {
         println("Nombre: $nombre, Vida: $vida, Ataque: $ataque")
@@ -26,8 +26,42 @@ fun ej05() {
 }
 
 
+fun ej07() {
+    val operacion: (Int, Int) -> Int = { num1, num2 -> num1 + num2 }
+    println(operacion(3, 4))
+
+    fun multiplicar(num: Int): Int {
+        return num * 2
+    }
+    println(multiplicar(5))
+
+}
+
+
+fun ejFinal() {
+    val goku = Personaje("Goku", 100, 20)
+    val vegeta = Personaje("Vegeta", 80, 25)
+
+    while (goku.vida > 0 && vegeta.vida > 0) {
+        goku.vida -= vegeta.ataque
+        vegeta.vida -= goku.ataque
+        println("Vida de ${goku.nombre}: ${goku.vida}")
+        println("Vida de ${vegeta.nombre}: ${vegeta.vida}")
+    }
+
+    if (goku.vida <= 0) {
+        println("Vegeta ha ganado el combate")
+    } else {
+        println("Goku ha ganado el combate")
+    }
+
+}
+
+
 fun main() {
     ej01()
     ej03()
     ej05()
+    ej07()
+    ejFinal()
 }
